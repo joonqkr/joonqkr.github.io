8b110e5c41349e4093e3cebe062097f615deeae9
Joon Park <joonqkr@berkeley.edu>
2021-08-15 20:47:23 -0400

    section labels show when nav bar icons are hovered over

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -57,6 +57,22 @@ function edu() { change_section("education"); }
 function int() { change_section("interests"); }
 function proj() { change_section("projects"); }
 
+/* functions for hovering over each button */
+function about_hover() { show("about-text"); }
+function exp_hover() { show("exp-text"); }
+function ski_hover() { show("ski-text"); }
+function edu_hover() { show("edu-text"); }
+function int_hover() { show("int-text"); }
+function proj_hover() { show("proj-text"); }
+
+/* functions for hovering out of each button */
+function about_hover_out() { hide("about-text"); }
+function exp_hover_out() { hide("exp-text"); }
+function ski_hover_out() { hide("ski-text"); }
+function edu_hover_out() { hide("edu-text"); }
+function int_hover_out() { hide("int-text"); }
+function proj_hover_out() { hide("proj-text"); }
+
 /* what happens when each button is clicked */
 exitButton.onclick = close_all;
 aboutButton.onclick = about;
@@ -65,3 +81,19 @@ skiButton.onclick = ski;
 eduButton.onclick = edu;
 interButton.onclick = int;
 projButton.onclick = proj;
+
+/* what happens when each section button is hovered */
+aboutButton.onmouseover = about_hover;
+expButton.onmouseover = exp_hover;
+skiButton.onmouseover = ski_hover;
+eduButton.onmouseover = edu_hover;
+interButton.onmouseover = int_hover;
+projButton.onmouseover = proj_hover;
+
+/* what happens when each section button is hovered out of */
+aboutButton.onmouseout = about_hover_out;
+expButton.onmouseout = exp_hover_out;
+skiButton.onmouseout = ski_hover_out;
+eduButton.onmouseout = edu_hover_out;
+interButton.onmouseout = int_hover_out;
+projButton.onmouseout = proj_hover_out;
